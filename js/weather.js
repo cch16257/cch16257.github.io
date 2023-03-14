64a3719a1ff32d4a1b164be8169fadf5e891d381
cch16257 <1625748085@qq.com>
2023-03-14 23:27:12 +0800

    Site updated: 2023-03-14 23:27:07

---
+++ js/weather.js
@@ -0,0 +1,14 @@
+<div id="he-plugin-standard"></div>
+<script>
+WIDGET = {
+  "CONFIG": {
+    "layout": "1",
+    "width": "450",
+    "height": "150",
+    "background": "1",
+    "dataColor": "FFFFFF",
+    "key": "a82c8c08fd0f492bb1a1591e05c801ac"
+  }
+}
+</script>
+<script src="https://widget.qweather.net/standard/static/js/he-standard-common.js?v=2.0"></script>
